7e64a324c2a5eea958e4be3d62647807ddff1141
dariakoval <dasharibkavmore666@mail.ru>
2023-11-03 16:58:12 +0400

    update files

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
+	id("com.github.ben-manes.versions") version "0.49.0"
 	id("io.freefair.lombok") version "8.4"
 }
 
@@ -17,8 +18,18 @@ repositories {
 }
 
 dependencies {
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql:42.6.0")
+
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("net.datafaker:datafaker:2.0.1")
+	implementation("org.instancio:instancio-junit:3.3.0")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -34,5 +45,14 @@ checkstyle {
 tasks.jacocoTestReport {
 	reports {
 		xml.required = true
+	}
+}
+
+tasks {
+	val stage by registering {
+		dependsOn(clean, installDist)
+	}
+	installDist {
+		mustRunAfter(clean)
 	}
 }
