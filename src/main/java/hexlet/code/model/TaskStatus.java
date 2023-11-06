436bfec41c49d1110c33343273d6b042b215ca8d
dariakoval <dasharibkavmore666@mail.ru>
2023-11-06 17:55:30 +0400

    create model TaskStatus

---
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -0,0 +1,41 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "task_statuses")
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class TaskStatus implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank
+    private String name;
+
+    @Column(unique = true)
+    @NotBlank
+    private String slug;
+
+    @CreatedDate
+    private Date createdAt;
+}
