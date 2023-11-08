e8f0be011da524cb70bea14afe986038d925c986
dariakoval <dasharibkavmore666@mail.ru>
2023-11-08 17:00:37 +0400

    move operation logic for entity User to UserService.java

---
+++ src/main/java/hexlet/code/service/UserService.java
@@ -0,0 +1,77 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.user.UserCreateDTO;
+import hexlet.code.dto.user.UserDTO;
+import hexlet.code.dto.user.UserUpdateDTO;
+import hexlet.code.exception.MethodNotAllowedException;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.UserMapper;
+import hexlet.code.repository.TaskRepository;
+import hexlet.code.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<UserDTO> getAll() {
+        var users = userRepository.findAll();
+        return users.stream()
+                .map(userMapper::map)
+                .toList();
+    }
+
+    public UserDTO create(UserCreateDTO userData) {
+        var user = userMapper.map(userData);
+        var hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPasswordDigest(hashedPassword);
+        userRepository.save(user);
+        return userMapper.map(user);
+    }
+
+    public UserDTO findById(Long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id %s not found", id)));
+        return userMapper.map(user);
+    }
+
+    public UserDTO update(UserUpdateDTO userData, Long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id %s not found", id)));
+        userMapper.update(userData, user);
+
+        if (userData.getPasswordDigest() == null) {
+            userRepository.save(user);
+        } else {
+            String hashedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPasswordDigest(hashedPassword);
+            userRepository.save(user);
+        }
+
+        return userMapper.map(user);
+    }
+
+    public void delete(Long id) {
+        var userTasks = userRepository.findById(id).get().getTasks();
+
+        if (userTasks.isEmpty()) {
+            userRepository.deleteById(id);
+        } else {
+            throw new MethodNotAllowedException("Operation not possible");
+        }
+    }
+}
