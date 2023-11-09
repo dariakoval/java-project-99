4935876f91e11ea9691ed9daa214073ab7c26a6f
dariakoval <dasharibkavmore666@mail.ru>
2023-11-09 13:18:15 +0400

    create model Label.java

---
+++ src/main/java/hexlet/code/model/Label.java
@@ -0,0 +1,34 @@
+package hexlet.code.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "labels")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Label implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 1000)
+    private String name;
+
+    @CreatedDate
+    private Date createdAt;
+}
