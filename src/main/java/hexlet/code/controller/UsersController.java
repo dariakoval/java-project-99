2a94afe7b5762fa674abfbe61ad909655afae212
dariakoval <dasharibkavmore666@mail.ru>
2023-11-10 15:20:02 +0400

    update UsersController.java: add swagger annotations

--- src/main/java/hexlet/code/controller/UsersController.java
+++ src/main/java/hexlet/code/controller/UsersController.java
@@ -3,7 +3,16 @@ package hexlet.code.controller;
 import hexlet.code.dto.user.UserCreateDTO;
 import hexlet.code.dto.user.UserDTO;
 import hexlet.code.dto.user.UserUpdateDTO;
+import hexlet.code.model.User;
 import hexlet.code.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,6 +32,7 @@ import java.util.List;
 
 import static hexlet.code.controller.UsersController.USER_CONTROLLER_PATH;
 
+@Tag(name = "Users controller", description = "Interaction with users")
 @RestController
 @RequestMapping("${base-url}" + USER_CONTROLLER_PATH)
 @AllArgsConstructor
@@ -38,12 +48,27 @@ public class UsersController {
 
     private final UserService userService;
 
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "Get specific user by his id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the user",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "User with that id not found",
+                    content = @Content) })
     @GetMapping(ID)
     @ResponseStatus(HttpStatus.OK)
-    public UserDTO show(@PathVariable Long id) {
+    public UserDTO show(
+            @Parameter(description = "Id of user to be searched")
+            @PathVariable Long id) {
         return userService.findById(id);
     }
 
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "Get list of all users")
+    @ApiResponse(responseCode = "200", description = "List of all users",
+            content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserDTO.class)) })
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<UserDTO>> index() {
@@ -53,22 +78,53 @@ public class UsersController {
                 .body(users);
     }
 
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "Create new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid user data supplied",
+                    content = @Content) })
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDTO create(@Valid @RequestBody final UserCreateDTO userData) {
+    public UserDTO create(
+            @Parameter(description = "User data to save")
+            @Valid @RequestBody final UserCreateDTO userData) {
         return userService.create(userData);
     }
 
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "Update user by his id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid user data supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "User with that id not found") })
     @PutMapping(ID)
     @ResponseStatus(HttpStatus.OK)
-    public UserDTO update(@RequestBody @Valid UserUpdateDTO userData, @PathVariable Long id) {
+    public UserDTO update(
+            @Parameter(description = "User data to update")
+            @RequestBody @Valid UserUpdateDTO userData,
+            @Parameter(description = "Id of user to be updated")
+            @PathVariable Long id) {
         return userService.update(userData, id);
     }
 
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "Delete user by his id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted", content = @Content),
+            @ApiResponse(responseCode = "405", description = "Operation not possible", content = @Content)
+    })
     @DeleteMapping(ID)
     @PreAuthorize(ONLY_OWNER_BY_ID)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void destroy(@PathVariable Long id) {
+    public void destroy(
+            @Parameter(description = "Id of user to be deleted")
+            @PathVariable Long id) {
         userService.delete(id);
     }
 }
