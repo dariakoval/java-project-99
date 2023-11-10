141020e1e7c9b6414e7ea679b174804852b4b372
dariakoval <dasharibkavmore666@mail.ru>
2023-11-10 04:20:25 +0400

    move operation logic for entity Label to LabelService.java

---
+++ src/main/java/hexlet/code/service/LabelService.java
@@ -0,0 +1,74 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.label.LabelCreateDTO;
+import hexlet.code.dto.label.LabelDTO;
+import hexlet.code.dto.label.LabelUpdateDTO;
+import hexlet.code.exception.MethodNotAllowedException;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.LabelMapper;
+import hexlet.code.repository.LabelRepository;
+import hexlet.code.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LabelService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
+    private LabelMapper labelMapper;
+
+
+    public List<LabelDTO> getAll() {
+        var labels = labelRepository.findAll();
+        return labels.stream()
+                .map(labelMapper::map)
+                .toList();
+    }
+
+    public LabelDTO create(LabelCreateDTO labelData) {
+        var label = labelMapper.map(labelData);
+        labelRepository.save(label);
+
+        return labelMapper.map(label);
+    }
+
+    public LabelDTO findById(Long id) {
+        var label = labelRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Label with id %s not found", id)));
+        return labelMapper.map(label);
+    }
+
+    public LabelDTO update(LabelUpdateDTO labelData, Long id) {
+        var label = labelRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Label with id %s not found", id)));
+        labelMapper.update(labelData, label);
+        labelRepository.save(label);
+
+        return labelMapper.map(label);
+    }
+
+    public void delete(Long id) {
+        var tasks = taskRepository.findAll();
+        Optional<Long> match = tasks.stream()
+                .map(task -> task.getLabels())
+                .flatMap(labels -> labels.stream())
+                .map(label -> label.getId())
+                .filter(i -> i.equals(id))
+                .findAny();
+
+        if (match.isEmpty()) {
+            labelRepository.deleteById(id);
+        } else {
+            throw new MethodNotAllowedException("Operation not possible");
+        }
+    }
+}
