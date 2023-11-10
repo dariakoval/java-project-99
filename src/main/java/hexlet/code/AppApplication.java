8096980dc172a19d320ce61e47c8b23d8e462d7c
dariakoval <dasharibkavmore666@mail.ru>
2023-11-10 15:23:23 +0400

    update AppApplication.java: add swagger annotations

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,11 +1,33 @@
 package hexlet.code;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task Manager",
+                version = "1.0",
+                description = "Task management system",
+                contact = @Contact(
+                        name = "Darya Koval",
+                        url = "https://github.com/dariakoval"
+                )
+        )
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 @SpringBootApplication
 @EnableJpaAuditing
 public class AppApplication {
