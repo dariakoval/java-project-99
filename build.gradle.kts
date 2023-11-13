76da37e3394b44f5d0d3216aaf0c7d72247dd819
dariakoval <dasharibkavmore666@mail.ru>
2023-11-13 18:39:26 +0400

    сonnect the Sentry error handler

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.3"
 	id("com.github.ben-manes.versions") version "0.49.0"
 	id("io.freefair.lombok") version "8.4"
+	id("io.sentry.jvm.gradle") version "3.14.0"
 }
 
 group = "hexlet.code"
@@ -13,34 +14,51 @@ version = "0.0.1-SNAPSHOT"
 
 application { mainClass.set("hexlet.code.AppApplication") }
 
+buildscript {
+	repositories {
+		mavenCentral()
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
+sentry {
+	includeSourceContext.set(true)
+
+	org.set("darya-koval")
+	projectName.set("java-spring-boot-task-manager")
+	authToken.set(System.getenv("SENTRY_AUTH_TOKEN"))
+}
+
 dependencies {
-	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.h2database:h2:2.1.214")
 	runtimeOnly("org.postgresql:postgresql:42.6.0")
 
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.0.4")
+	implementation("org.springframework.boot:spring-boot-devtools:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
 	implementation("net.datafaker:datafaker:2.0.1")
 	implementation("org.instancio:instancio-junit:3.3.0")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:3.1.0")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-
 
 	testImplementation(platform("org.junit:junit-bom:5.10.0"))
 	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
+	testImplementation("org.springframework.security:spring-security-test:6.0.2")
 
+}
+
+tasks.sentryBundleSourcesJava {
+	environment("SENTRY_AUTH_TOKEN", (System.getenv("SENTRY_AUTH_TOKEN")))
 }
 
 tasks.withType<Test> {
