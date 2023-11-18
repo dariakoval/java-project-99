bc99254d36c6454db351230c9cde619b9d957801
dariakoval <dasharibkavmore666@mail.ru>
2023-11-18 14:37:49 +0400

    create ModelGenerator.java

---
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -0,0 +1,56 @@
+package hexlet.code.util;
+
+import hexlet.code.model.Label;
+import hexlet.code.model.Task;
+import hexlet.code.model.TaskStatus;
+import hexlet.code.model.User;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.instancio.Model;
+import org.instancio.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class ModelGenerator {
+
+    private Model<User> userModel;
+    private Model<TaskStatus> taskStatusModel;
+    private Model<Label> labelModel;
+    private Model<Task> taskModel;
+
+    @Autowired
+    private Faker faker;
+
+    @PostConstruct
+    private void init() {
+        taskModel = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .supply(Select.field(Task::getIndex), () -> (Integer) faker.number().positive())
+                .supply(Select.field(Task::getTitle), () -> faker.lorem().word())
+                .supply(Select.field(Task::getContent), () -> faker.lorem().sentence())
+                .toModel();
+
+        labelModel = Instancio.of(Label.class)
+                .ignore(Select.field(Label::getId))
+                .supply(Select.field(Label::getName), () -> "new")
+                .toModel();
+
+        taskStatusModel = Instancio.of(TaskStatus.class)
+                .ignore(Select.field(TaskStatus::getId))
+                .supply(Select.field(TaskStatus::getName), () -> "To test")
+                .supply(Select.field(TaskStatus::getSlug), () -> "to_test")
+                .toModel();
+
+        userModel = Instancio.of(User.class)
+                .ignore(Select.field(User::getId))
+                .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
+                .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
+                .supply(Select.field(User::getLastName), () -> faker.name().lastName())
+                .supply(Select.field(User::getPasswordDigest), () -> faker.internet().password(3, 12))
+                .toModel();
+    }
+}
