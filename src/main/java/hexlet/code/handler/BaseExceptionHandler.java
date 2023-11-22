b722f1fee5e2755070ab5be1b4732bc7dc222e82
dariakoval <dasharibkavmore666@mail.ru>
2023-11-22 17:16:46 +0400

    update BaseExceptionHandler.java: add additional exception handlers

--- src/main/java/hexlet/code/handler/BaseExceptionHandler.java
+++ src/main/java/hexlet/code/handler/BaseExceptionHandler.java
@@ -2,22 +2,76 @@ package hexlet.code.handler;
 
 import hexlet.code.exception.MethodNotAllowedException;
 import hexlet.code.exception.ResourceNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
+@ResponseBody
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class BaseExceptionHandler {
 
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public String resourceNotFoundExceptionHandler(ResourceNotFoundException exception) {
+        return exception.getMessage();
     }
 
+    @ResponseStatus(METHOD_NOT_ALLOWED)
     @ExceptionHandler(MethodNotAllowedException.class)
-    public ResponseEntity<String> handleMethodNotAllowedException(MethodNotAllowedException ex) {
-        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(ex.getMessage());
+    public String methodNotAllowedExceptionHandler(MethodNotAllowedException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ObjectError> validationExceptionsHandler(MethodArgumentNotValidException exception) {
+        return exception.getAllErrors();
+    }
+
+    @ResponseStatus(UNAUTHORIZED)
+    @ExceptionHandler(AuthenticationException.class)
+    public String badCredentialsExceptionHandler(AuthenticationException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public String accessDeniedException(AccessDeniedException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public String dataIntegrityViolationExceptionsHandler(DataIntegrityViolationException exception) {
+        return exception.getCause().getCause().getMessage();
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public String validationExceptionsHandler(HttpMessageNotReadableException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String generalExceptionHandler(Exception exception) {
+        return exception.getMessage();
     }
 }
